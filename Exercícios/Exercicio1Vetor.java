cec504643cc6cb0cfef6280e2561c53fd68a8d7e
Isadora Goedert <isadoragoedert05@gmail.com>
2025-06-26 09:15:10 -0300

    Mostrando 6 valores inteiros

--- Exercícios/Exercicio1Vetor.java
+++ Exercícios/Exercicio1Vetor.java
@@ -1,8 +1,24 @@
+/*1. Crie um programa que le 6 valores inteiros e, em seguida, mostre na tela os 
+valores lidos.
+*/
+
 import java.util.Scanner;
 
 public class Exercicio1Vetor {
     public static void main(String[] args){
-        int number
+        String[] number = new String[6];
+        Scanner scanner = new Scanner(System.in);
+        int i; 
+
+        for( i = 0; i < 6; i++){
+            System.out.println("Digite o " +(i + 1)+"° numero:");
+            number[i] = scanner.nextLine();
+        }
+        for( i = 0; i < 6; i++){
+            System.out.print(number[i]+ " | ");
+        }
+        
+
     }
 
     
