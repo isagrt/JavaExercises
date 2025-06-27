f81d97d06f209704b64da6c5b36199c7b4081bc1
Isadora Goedert <isadoragoedert05@gmail.com>
2025-06-27 08:49:29 -0300

    adicionando exercício 3 de vetores

---
+++ Exercícios/Exercicio3Vetor.java
@@ -0,0 +1,31 @@
+/* 3. Faça um programa que receba do usuário um vetor com 10 posições. Em seguida 
+deverá ser impresso o maior e o menor elemento do vetor. */
+
+import java.util.Scanner;
+
+public class Exercicio3Vetor{
+    public static void main(String[] args){
+        int[] number = new int[10];
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o 1° numero: ");
+        number[0] = scanner.nextInt();
+         int maior = number[0];
+        int menor = number[0];      
+
+        for( int i = 1; i < 10; i++){
+
+            System.out.println("Digite o "+(i + 1)+"° numero: ");
+            number[i] = scanner.nextInt();
+
+            if( number[i]>maior){
+                maior = number[i];
+            }
+            if( number[i]<menor){
+                menor = number[i];
+            }       
+        }
+         System.out.println("O maior numero da lista é: " +maior);
+            System.out.println("O menor numero da lista é: " +menor);
+    }
+}// alterar para que consiga imprimir o menor numero
