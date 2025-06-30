a46b3e79f0c768143b78757a11a3d575da59f215
Isadora Goedert <isadoragoedert05@gmail.com>
2025-06-30 11:27:41 -0300

    resolvendo erros de logica

--- Exercícios/Exercicio3Matriz.java
+++ Exercícios/Exercicio3Matriz.java
@@ -8,14 +8,27 @@ public class Exercicio3Matriz {
     public static void main(String[] args) {
         double[][] matriz = new double[4][4];
 
-            for (int i = 0; i <4; i++){
+        for (int i = 0; i < 4; i++) {
             for (int y = 0; y < 4; y++) {
-            matriz[i][y] = (int) (Math.random() * 20);
-            System.out.print(matriz[i][y]+ "  ");
-            
+                matriz[i][y] = (int) (Math.random() * 20);
+                System.out.print(matriz[i][y] + "  ");
             }
-            System.out.println(" ");
+            System.out.println();
+        }
+        System.out.print("\nDiagonal principal: ");
+        for (int i = 0; i < 4; i++) {
+            System.out.print(matriz[i][i] + " | ");
+        }
+        System.out.print("\nDiagonal secundaria: ");
+        for (int i = 0; i < 4; i++) {
+            System.out.print(matriz[i][3 - i] + " | ");
+        }
+        System.out.println("\n\nMatriz transposta:");
+        for (int i = 0; i < 4; i++) {
+            for (int y = 0; y < 4; y++) {
+                System.out.print(matriz[y][i] + "  |  ");
             }
-
-
-    }}
+            System.out.println();
+        }
+    }
+}
