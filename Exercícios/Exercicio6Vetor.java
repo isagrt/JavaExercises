718cfcb7753e720410d93312ff2b3a14b5ac9034
Isadora Goedert <isadoragoedert05@gmail.com>
2025-06-30 08:10:16 -0300

    arrumando erros vetores

--- Exercícios/Exercicio6Vetor.java
+++ Exercícios/Exercicio6Vetor.java
@@ -1,32 +1,31 @@
-/*6. Faça um programa que receba do usuário dois vetores, A e B, com 10 números 
-inteiros cada. Crie um novo vetor denominado C calculando C = A - B. Mostre na 
-tela os dados do vetor C*/
-
 import java.util.Scanner;
 
-public class Exercicio6Vetor{
-    public static void main(String[]args){
-        String[] A = new String[10];
-        String[] B = new String[10];
-        String[] C = new String[10];
-        String[] soma = new String[10];
+public class Exercicio6Vetor {
+    public static void main(String[] args) {
+        int[] A = new int[10];
+        int[] B = new int[10];
+        int[] C = new int[10];
         Scanner scanner = new Scanner(System.in);
 
-        for(int i = 0; i < 10; i++ ){
-            Sistem.out.println("Digite um valor para o primeiro vetor: ");
-            A[i] = scanner.nextString();
-        }
-        
-        for(int i = 0; i < 10; i++ ){
-            Sistem.out.println("Digite um valor para o segundo vetor: ");
-            B[i] = scanner.nextString();
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Digite um valor para o vetor A[" + i + "]: ");
+            A[i] = scanner.nextInt();
         }
 
-        C[i] = A[i] + B[i]; //erro, alterar!!
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Digite um valor para o vetor B[" + i + "]: ");
+            B[i] = scanner.nextInt();
+        }
 
-        System.out.println("Vetor A + Vetor B = "+C+" | ");
+        for (int i = 0; i < 10; i++) {
+            C[i] = A[i] - B[i];
+        }
 
-        
+        System.out.println("\nVetor C (A - B):");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("C[" + i + "] = " + C[i]);
+        }
+
+        scanner.close();
     }
-
 }
