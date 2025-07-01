41f5f5e6138b3d9ec0ab20d77e5a787a4ebb96da
Isadora Goedert <isadoragoedert05@gmail.com>
2025-07-01 10:15:21 -0300

    Organizando pastas

---
+++ Metodos/Metodo01.java
@@ -0,0 +1,16 @@
+public class Metodo01{ //miniminiza todo codigo
+
+    static void myMethod(){ //cria o metodo 
+
+        System.out.println("Ola, voce chamou o metodo simples"); 
+    }
+
+    public static void main(String[] args){
+
+        System.out.println("Chamda de metodo simples:");
+        myMethod();
+        myMethod();
+        myMethod(); //chama o metodo criado
+    }
+}
+
