261a1ed563f4e532662e80fb75422b4b75450bd0
Isadora Goedert <isadoragoedert05@gmail.com>
2025-07-01 08:19:09 -0300

    alterando para exibir matriz corretamente

--- Exercícios/Exercicio4Matriz.java
+++ Exercícios/Exercicio4Matriz.java
@@ -1,34 +1,45 @@
 import java.util.Scanner;
-
-public class Exercicio4Matriz {
+public class Exercicio4Matriz{
+ 
     public static void main(String[] args) {
-        char[][] matriz = new char [4][4]; 
-        
-         for (int i = 0; i < 4; i++) {
-            for (int y = 0; y < 4; y++) {
-                matriz[i][y]= '*';
+ 
+        Scanner ler = new Scanner(System.in);
+ 
+        char [][] matriz = new char[4][4];
+ 
+       // Preencher matriz
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                matriz[i][j] = '*';
             }
         }
-        for (int i = 0; i < 4; i++) {
-            for (int y = 0; y < 4; y++) {
-                System.out.print(matriz[i][y]+ "  |  ");
+ 
+        // Matriz
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < 4; j++){
+                System.out.print("\t"+matriz[i][j]);
             }
-            System.out.println(" "); 
-
-}
-    for (int i = 0; i < 4; i++) {
-            for (int y = 0; y < 4; y++) {
-               if(i==0 || i == 3 || y == 0 || y == 3) {
-                System.out.print(matriz[i][y]+ "  |  ");
-               }
-               else{
-                System.out.println("\t ");
-               }
+            System.out.println("");
+        }
+ 
+        System.out.println("");
+        // Bordas
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < 4; j++){
+                if (i == 0 || i == 3 || j == 0 || j == 3)
+                    System.out.print("\t"+matriz[i][j]);
+                else
+                    System.out.print("\t");
+            }
+             System.out.println("");
+        }
+             System.out.println("");
+            // Triangulo inferior
+             for(int i = 0; i < 4; i++){
+                for(int j = 0; j <= i; j++){
+                    System.out.print("\t"+matriz[i][j]);
+                }
+                System.out.println("");
              }
-              System.out.println(" ");
-    
-}  System.out.println(" ");
-
-    for (int i = 0; i < 4; i++) {
-            for (int y = 0; y < 4; y++) {
-}}
+        }
+    }
