22fe3641573e83528fc1133783ae888a1cb60861
Isadora Goedert <isadoragoedert05@gmail.com>
2025-07-02 11:54:06 -0300

    inserindo se há vencedor

--- Metodos/MetodoTicTacToe.java
+++ Metodos/MetodoTicTacToe.java
@@ -1,72 +1,83 @@
-/* Jogo da Velha*/
 import java.util.Scanner;
 
-public class MetodoTicTacToe{
-    public static char[][] jogoDaVelha(char[][] matriz){
-        
-            for (int x = 0; x<3; x++){
-                for(int o = 0; o<3; o++ ){
-                    System.out.print(" "+matriz[x][o]+" ");
-                    if(o<2){
-                        System.out.print("|");
-                    }
+public class MetodoTicTacToe {
+
+    public static char[][] jogoDaVelha(char[][] matriz) {
+        for (int x = 0; x < 3; x++) {
+            for (int o = 0; o < 3; o++) {
+                System.out.print(" " + matriz[x][o] + " ");
+                if (o < 2) {
+                    System.out.print("|");
                 }
-                System.out.println();
-                if(x<2){
-                    System.out.print("");
-                }
-            } 
-            return matriz;
+            }
+            System.out.println();
+            if (x < 2) {
+                System.out.println("---+---+---");
+            }
+        }
+        return matriz;
     }
 
-    public static char alternarJogador(){
+    public static void alternarJogador() {
         Scanner scanner = new Scanner(System.in);
         boolean jogoAtivo = true;
-        String jogadorAtual = "X";
+        char jogadorAtual = 'X';
         char[][] matriz = new char[3][3];
 
-        for(int x = 0; x < 3; x++){
-                for(int o = 0; o < 3; o++){
-                    matriz[x][o] = ' ';
-                }
+        
+        for (int x = 0; x < 3; x++) {
+            for (int o = 0; o < 3; o++) {
+                matriz[x][o] = ' ';
+            }
+        }
 
+        
         while (jogoAtivo) {
-    imprimirTabuleiro(matriz);
-    System.out.println("Vez do jogador " + jogadorAtual);
+            jogoDaVelha(matriz);
+            System.out.println("Vez do jogador " + jogadorAtual);
 
-    System.out.print("Linha (0-2): ");
-    int linha = scanner.nextInt();
-    System.out.print("Coluna (0-2): ");
-    int coluna = scanner.nextInt();
+            System.out.print("Linha (0-2): ");
+            int linha = scanner.nextInt();
+            System.out.print("Coluna (0-2): ");
+            int coluna = scanner.nextInt();
 
-    if (linha < 0 || linha > 2 || coluna < 0 || coluna > 2) {
-        System.out.println("Posição inválida! Tente novamente.");
-        continue;
+            if (linha < 0 || linha > 2 || coluna < 0 || coluna > 2) {
+                System.out.println("Posição inválida! Tente novamente.");
+                continue;
+            }
+
+            if (matriz[linha][coluna] != ' ') {
+                System.out.println("Essa casa já está ocupada!");
+                continue;
+            }
+
+            matriz[linha][coluna] = jogadorAtual;
+
+        
+            if (jogadorAtual == 'X') {
+                jogadorAtual = 'O';
+            } else {
+                jogadorAtual = 'X';
+            }
+        }
     }
 
-    if (matriz[linha][coluna] != ' ') {
-        System.out.println("Essa casa já está ocupada!");
-        continue;
-    }
+    public static boolean haVencedor(char[][] jogoDaVelha, char jogador){
+        for (int i = 0; i<3; i++){
+            if (jogoDaVelha[i][0] == jogador && jogoDaVelha[i][1] == jogador && jogoDaVelha[i][2] == jogador){
+                return true;
+            }
+        }
+        for (int j = 0; j<3; j++){
+            if (jogoDaVelha[0][j] == jogador && jogoDaVelha[1][j] == jogador && jogoDaVelha[2][j] == jogador){
+                return true;
+            }
 
-    matriz[linha][coluna] = jogadorAtual;
-    
-    if (jogadorAtual == 'X') {
-        jogadorAtual = 'O';
-    } else {
-        jogadorAtual = 'X';
+        }
+        return false;
+        }
+
+    public static void main(String[] args) {
+        alternarJogador();
     }
 }
-        
-    }
-        
-
-    
-
-    public static void main(String[] args){
-        jogoDaVelha();
-        alternarJogador();
-
-    }
-
-}}
